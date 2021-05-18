f32b20d7092eec554f46da086d361a3f7a5d4fb1
tanhao <295593206@qq.com>
2021-05-18 22:57:36 +0800

    ‘first commit

---
+++ src/main/java/com/baizhi/EmsVueApplication.java
@@ -0,0 +1,17 @@
+package com.baizhi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@SpringBootApplication
+public class EmsVueApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmsVueApplication.class, args);
+    }
+
+
